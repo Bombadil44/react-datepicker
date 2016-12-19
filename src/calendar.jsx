9e2d3bae848b3d1cd89687ee9902bff77683c203
Bombadil44 <tom.mastwyk@au.ey.com>
2016-12-19 13:49:10 +1100

    test commit

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -6,6 +6,7 @@ import Month from './month'
 import React from 'react'
 import { isSameDay, allDaysDisabledBefore, allDaysDisabledAfter, getEffectiveMinDate, getEffectiveMaxDate } from './date_utils'
 
+// test
 const DROPDOWN_FOCUS_CLASSNAMES = [
   'react-datepicker__year-select',
   'react-datepicker__month-select'
